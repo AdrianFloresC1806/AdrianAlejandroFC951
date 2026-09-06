b6d239eb87d0f5cfcc3d3b0cb873e14062dc1352
Adrian A. Flores C. <a2219209@uabc.edu.mx>
2026-09-06 13:38:09 -0700

    Tareas

--- Meta 1.3 Scraping con Selenium/Meta 1.3 Scraping con Selenium.py
+++ Meta 1.3 Scraping con Selenium/Meta 1.3 Scraping con Selenium.py
@@ -1,3 +1,12 @@
+# Flores Cisneros Adrian Alejandro
+# 951
+# Se realizó el 05/09/2026
+#Este script usa Selenium para automatizar búsquedas de productos en MercadoLibre. Para cada producto, escribe el
+# término en el buscador, confirma con Enter y toma una captura de los resultados. Luego localiza el botón de la página
+# deseada mediante su atributo aria-label (con XPath que me ayuda a buscar el atributo de forma "diferente"),
+# hace clic y captura esa segunda vista. Se usan esperas explícitas (WebDriverWait) para asegurar que cada elemento
+# exista antes de interactuar con él y que este no salte un error.
+
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -8,7 +17,45 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def luz(producto):
+def luz(producto,num_pag):
+
+    url="https://www.mercadolibre.com.mx/"
+    s=Service(ChromeDriverManager().install())
+    opc=Options()
+    opc.add_argument('--window-size=1020x800')
+    navegador=webdriver.Chrome(service=s, options=opc)
+
+    wait=WebDriverWait(navegador,10)
+
+    navegador.get(url)
+    time.sleep(2)
+
+    txtuser=wait.until(
+        EC.presence_of_element_located((By.ID, "cb1-edit"))
+    )
+
+    buttonsearch=wait.until(
+        EC.element_to_be_clickable((By.CLASS_NAME, "nav-search-btn"))
+    )
+    txtuser.send_keys(producto)
+    time.sleep(2)
+    buttonsearch.send_keys(Keys.ENTER)
+    time.sleep(2)
+
+    buttonsearchnum=wait.until(
+        EC.element_to_be_clickable((By.XPATH, f"//a[@aria-label='Ir a la página {num_pag}']"))
+    )
+    #el xpath busca de forma general (id, Class, etc) en este caso para buscar un atributo con un identificador unico
+    # aria-label cumple con lo que yo queria hacer ## //a busca la etiqueta con la estructura [@atributo = 'descripcion']
+
+    buttonsearchnum.send_keys(Keys.ENTER)
+    time.sleep(4)
+    navegador.save_screenshot("Meta 1.3 pLuz.png")
+    time.sleep(2)
+    navegador.close()
+
+def proyectores(producto,num_pag):
+
     url="https://www.mercadolibre.com.mx/"
     s=Service(ChromeDriverManager().install())
     opc=Options()
@@ -31,42 +78,26 @@ def luz(producto):
 
     txtuser.send_keys(producto)
     time.sleep(2)
-    txtuser.send_keys(Keys.ENTER)
-    time.sleep(2)
-    navegador.save_screenshot("M1.3.png")
-    time.sleep(2)
-    navegador.close()
-
-def proyectores(producto):
-    url="https://www.mercadolibre.com.mx/"
-    s=Service(ChromeDriverManager().install())
-    opc=Options()
-    opc.add_argument('--window-size=1020x800')
-    navegador=webdriver.Chrome(service=s, options=opc)
-
-    wait=WebDriverWait(navegador,10)
-
-    navegador.get(url)
+    buttonsearch.send_keys(Keys.ENTER)
     time.sleep(2)
 
-    txtuser=wait.until(
-        EC.presence_of_element_located((By.ID, "cb1-edit"))
-    )
+    if num_pag > 5:
+        print(f"La pagina numero {num_pag} no existe en el apartado de {producto}")
 
-    buttonsearch=wait.until(
-        EC.element_to_be_clickable((By.CLASS_NAME, "nav-search-btn"))
-    )
+    else:
 
+        buttonsearchnum=wait.until(
+            EC.element_to_be_clickable((By.XPATH, f"//a[@aria-label='Ir a la página {num_pag}']"))
+        )
 
-    txtuser.send_keys(producto)
-    time.sleep(2)
-    txtuser.send_keys(Keys.ENTER)
-    time.sleep(2)
-    navegador.save_screenshot("M1.3-Proyector.png")
-    time.sleep(2)
-    navegador.close()
+        buttonsearchnum.send_keys(Keys.ENTER)
+        time.sleep(4)
+        navegador.save_screenshot("Meta 1.3 qProyector.png")
+        time.sleep(2)
+        navegador.close()
 
-def pantalla(producto):
+def pantalla(producto, num_pag):
+
     url = "https://www.mercadolibre.com.mx/"
     s = Service(ChromeDriverManager().install())
     opc = Options()
@@ -88,15 +119,26 @@ def pantalla(producto):
 
     txtuser.send_keys(producto)
     time.sleep(2)
-    txtuser.send_keys(Keys.ENTER)
+    buttonsearch.send_keys(Keys.ENTER)
     time.sleep(2)
-    navegador.save_screenshot("M1.3-Pantalla8k.png")
-    time.sleep(2)
-    navegador.close()
+
+    if num_pag > 6:
+        print(f"El numero de pagina {num_pag} no existe en el apartado de {producto}")
+
+    else:
+        buttonsearchnum=wait.until(
+            EC.element_to_be_clickable((By.XPATH, f"//a[@aria-label='Ir a la página {num_pag}']"))
+        )
+
+        buttonsearchnum.send_keys(Keys.ENTER)
+        time.sleep(4)
+        navegador.save_screenshot("Meta 1.3 rPantalla.png")
+        time.sleep(2)
+        navegador.close()
 
 
 
 if __name__ == "__main__":
-    luz("Lamp Girat")
-    proyectores("Proyector 8k")
-    pantalla("Pantalla para Proyector 8k")
+    luz("Lamp Girat",6)
+    proyectores("Proyector 8k",5)
+    pantalla("Pantalla para proyector enrollable", 3)
